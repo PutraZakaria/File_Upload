4a22e58ea9ee5fa352d1544c93c7eafb621049fc
PutraZakaria <p1578003@gmail.com>
2024-05-14 10:29:46 +0700

    JS12: E. MEMBUAT SYMLINK

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -41,11 +41,24 @@ class FileUploadController extends Controller
         // echo $request->berkas->getClientOriginalName()."lolos validasi";
 
         // D
+        // $request->validate([
+        //     'berkas' => 'required|file|image|max:500',
+        // ]);
+        // $namaFile = $request->berkas->getClientOriginalName();
+        // $path = $request->berkas->storeAs('uploads', $namaFile);
+        // echo "proses upload berhasil, file berada di: $path";
+
+        // E
         $request->validate([
             'berkas' => 'required|file|image|max:500',
         ]);
-        $namaFile = $request->berkas->getClientOriginalName();
-        $path = $request->berkas->storeAs('uploads', $namaFile);
-        echo "proses upload berhasil, file berada di: $path";
+        $extfile=$request->berkas->getClientOriginalName();
+        $namaFile='web-' .time().".".$extfile;
+        $path = $request->berkas->storeAs('public', $namaFile);
+
+        $pathBaru = asset('storage/'.$namaFile);
+        echo "proses upload berhasil, data disimpan pada:$path";
+        echo "<br>";
+        echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
     }
 }
